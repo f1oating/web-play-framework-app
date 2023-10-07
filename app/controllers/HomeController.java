9bc72a65f3b4e6490667b953595c4cdc205f6dc6
f1oating <dtoronchenko@gmail.com>
2023-10-07 16:47:07 +0100

    third commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import play.libs.Files;
 import play.mvc.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HomeController extends Controller {
     public Result home(){
@@ -12,5 +16,24 @@ public class HomeController extends Controller {
 
     public Result contacts(){
         return ok(views.html.contacts.render());
+    }
+
+    public Result projects(){
+        return ok(views.html.projects.render());
+    }
+
+    public Result upload(Http.Request request) {
+        Http.MultipartFormData<Files.TemporaryFile> body = request.body().asMultipartFormData();
+        Http.MultipartFormData.FilePart<Files.TemporaryFile> picture = body.getFile("picture");
+        if (picture != null) {
+            String fileName = picture.getFilename();
+            long fileSize = picture.getFileSize();
+            String contentType = picture.getContentType();
+            Files.TemporaryFile file = picture.getRef();
+            file.copyTo(Paths.get("upload/dest.jpg"), true);
+            return ok("File uploaded");
+        } else {
+            return badRequest().flashing("error", "Missing file");
+        }
     }
 }
